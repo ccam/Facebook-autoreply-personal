62a6aba75d89addedf8e2cc8e9a0eef41ece3cb5
ccam <cam@cameronmcdonald.me>
2018-10-14 20:05:42 -0400

    changed where the user login info is.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Facebook away message
 
 Auto replies to all messages sent to your personal profile. I want to add a exclued list instead of just one converstaion, thats for personal use. 
+
+# Set up
+
+```shell
+npm install
+```
+
+You will need to make a json file named creds.json or just insert your email and password into the email and password variables. 
+
+```json
+{
+  "user" : {
+      "email" : "dankmaster6969696@rocketmail.pizza",
+      "password" : "Password123!"
+  }
+}
+
+```
--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-var login = require('facebook-chat-api');
+const login = require('facebook-chat-api');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+let conf = JSON.parse(fs.readFileSync('creds.json'))
+const creds = {
+  email: conf.user.email,
+  password: conf.user.password
+}
+let answeredThreads = {};
+let exclued = '' // TODO 1: Lneed to make into an array and loop over it
 
-var answeredThreads = {};
-
-let exclued = '' // need to make into an array and loop over it
 login({
-	email: 'dankmaster69696@rocketmail.pizza',
-	password: 'password123'
-}, function callback(err, api) {
+	email: creds.email,
+	password: creds.password 
+},function callback(err, api) {
+   
+  api.setOptions({logLevel: "silent"});
+
 	if (err) {
     switch(err.error) {
       case 'login-approval': // if you have login approvals turned on this will just re-enter you password so you don't have to approve it multiple times.
-        err.continue('password123');
+        err.continue(creds.password);
         break;
       default:
         console.error(err);
     }
-
+    
     return;
   }
 
 	api.listen(function callback(err, message) {
 		console.log(message.threadID);
-    if (message.threadID != exclued) { //should be for loop? to loop through exclued array.
+    if (message.threadID != exclued) { // TODO 1: should be for loop? to loop through exclued array.
 
 		  if (!answeredThreads.hasOwnProperty(message.threadID)) {
 		  	answeredThreads[message.threadID] = true; // This line only allows this bot/app/script to respond once. If it's removed the bot/app/script will respond to each message sent. 
